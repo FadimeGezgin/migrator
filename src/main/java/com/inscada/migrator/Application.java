ac494b7928b2a7fa1108512994762663b60d1809
Fadime <fadimegezgin03@gmail.com>
2019-07-09 11:23:48 +0300

    Initial commit

---
+++ src/main/java/com/inscada/migrator/Application.java
@@ -0,0 +1,13 @@
+
+package com.inscada.migrator;
+
+/**
+ *
+ * @author fadime
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
