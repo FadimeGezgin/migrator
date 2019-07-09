40c84759d56c2b793a3d0ad05f9fd7caf6732186
Fadime <fadimegezgin03@gmail.com>
2019-07-09 13:46:39 +0300

    different message

--- src/main/java/com/inscada/migrator/Application.java
+++ src/main/java/com/inscada/migrator/Application.java
@@ -8,6 +8,7 @@ package com.inscada.migrator;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello migrator");
+        System.out.println("Deneme testi");
     }
     
 }
