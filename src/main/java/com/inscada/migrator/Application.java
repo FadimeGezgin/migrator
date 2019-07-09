2a9639509b4ff648c1bddbed18da4779fe2c7d89
Fadime <fadimegezgin03@gmail.com>
2019-07-09 11:26:53 +0300

    change message

--- src/main/java/com/inscada/migrator/Application.java
+++ src/main/java/com/inscada/migrator/Application.java
@@ -7,7 +7,7 @@ package com.inscada.migrator;
  */
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello migrator");
     }
     
 }
